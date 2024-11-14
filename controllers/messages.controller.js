8b58f15c7a2c74aed8351ed1ad7d64c077074e43
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-11-14 11:17:11 -0500

    Create db.addMessage() and hook it up

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -16,11 +16,13 @@ const getMessages = asyncHandler(async (req, res) => {
 // create a new message and add it to the message array
 const sendMessage = asyncHandler(async (req, res) => {
   if (req.body.message === "") return res.redirect("back");
-  messageService.addMessage({
-    text: req.body.message,
-    user: store("user") || "User",
-    added: formatDate(new Date()),
-  });
+
+  await db.addMessage(
+    store("user") || "User",
+    req.body.message,
+    formatDate(new Date()),
+  );
+
   res.redirect("back");
 });
 
--- db/queries.js
+++ db/queries.js
@@ -1,11 +1,16 @@
 const pool = require("./pool");
 
 async function getAllMessages() {
-  const {rows} = await pool.query("SELECT * FROM messages");
+  const { rows } = await pool.query("SELECT * FROM messages");
   return rows;
 }
 
-async function addMessage() {}
+async function addMessage(username, message, time) {
+  await pool.query(
+    "INSERT INTO messages (username, message, time) VALUES ($1, $2, $3)",
+    [username, message, time],
+  );
+}
 
 async function deleteData() {}
 
